cf7eb7e721460ed3acbf9b3d5ac67341d4f84091
Mahsa-nvr <mahsaanvari2@gmail.com>
2020-03-17 16:02:14 +0330

    first commit

--- src/Alarm.js
+++ src/Alarm.js
@@ -1,36 +1,59 @@
 import React from 'react';
+class Alarm extends React.Component{
+    state = {
+        top: -100,
+        time: new Date()
+    };
 
-class Alarm extends React.Component {
-constructor(props) {
-    super(props)
-    this.state={
-        count: 0
+    componentDidMount() { 
+        setInterval(this.tick, 1000)
+        setTimeout(this.welcome, 3000)
     }
+    tick = () => {
+        this.setState({
+            time: new Date()
+        })
+    };
+
+
+    welcome = () => {
+        this.setState({
+            top: 93
+        }, () => {
+            setTimeout(() => {
+                this.setState({
+                    top: -100,
+                })
+            }, 5000)
+        })
+        
+    }
+   
+
+    render() {
+        
+        return (
+            <React.Fragment>
+                
+           <div>{this.state.time.toLocaleTimeString()}</div>
+           <div style={{
+               
+                 position: 'absolute',
+                backgroundColor:"#06d06b",
+                color: "white",
+                top: `${this.state.top}px` ,
+                right: "16px",
+                zIndex: 100,
+                transition: "top 0.5s ease",
+                width:"300px",
+                marginRight: "510px",
+                borderRadius: "3px"
+           }}>Welcome To App</div>
+           </React.Fragment>
+        );
+    }
+
+
 }
-
- render() {
-     const {count} = this.state
-     return (
-         <div>
-             <div>Current Count : {count}</div>
-         </div>
-     )
- }
-
-
- componentDidMount() {
-     this.myInterval = setInterval(() => {
-        this.setState(prevState => ({
-            count:  prevState.count + 1
-        }))
-     }, 1000)   
- }
-
- componentWillUnmount() {
-     clearInterval(this.myInterval)
- }
-
-}
-
 
 export default Alarm;
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, NavItem,NavLink, Row,  Input, Button, ListGroup, ListGroupItem} from 'reactstrap';
+import {  Row,  Input, Button, ListGroup, ListGroupItem} from 'reactstrap';
 import {HandleChange, Icon} from './Utility';
 import swal from 'sweetalert';
 import Alarm from './Alarm';
@@ -8,41 +8,57 @@ class App extends React.Component {
    constructor(props) {
      super(props)
      this.state = {
-       show: true,
+       top: -100,
        inputNew: '',
        data: [
-         {id:1, title:"one", statusDone: false},
-         {id:2, title:"two", statusDone: false}
+         {id:1, title:"meet mr.jack of all trads"},
+         {id:2, title:"being at my best mood"},
+         {id:2, title:"thinking about cosmos absurdity"}
        ]
      }
    }
 
    add = () => {
+     const value= this.state.inputNew.trim()
+     if(value !== ''){ 
      this.setState(prevState => {
       return {
         inputNew: '',
         data: [...prevState.data , {id: prevState.data.lenght + 1, title: prevState.inputNew}]
-      }
+      } 
      })
-   }
+   }else(alert("please enter character"))
+  }
 
-   delete = (index) => {
-     this.setState(prevState => {
-       let NewData = [...prevState.data]
-       NewData.splice(index, 1);
-       return {
-         data: NewData
-       }
-     })
-   }
+  delete = (index) => {
+    this.setState(prevState => {
+      prevState.top=16;
+      let NewData = [...prevState.data]
+      NewData.splice(index, 1);
+      return {
+        data: NewData
+      }
+    }, () => {
+     setTimeout(() => {
+       this.setState({
+           top: -100,
+       })
+   }, 3000)
+   })   
+  }
 
 
    edit = (index) => {
      swal({
        content: "input",
-     }).then(res => {
+     }).then(res => {  
        let NewState = this.state.data;
+       const value = res.trim();
+      if(value !== ''){
        NewState[index].title = res;
+     }else{
+       alert("please enter characters")
+     }
        this.setState({
          data: NewState
        })
@@ -51,75 +67,58 @@ class App extends React.Component {
      })
    }
   
-   done = (index) => {
-    this.setState(prevState => {
-      prevState.statusDone =! prevState.statusDone
-    })
-    console.log(this.state.statusDone)
-   }
+ 
 
 
   render() {
+    
     return (
      <React.Fragment>
-       <div><Alarm/></div>
+       
        <div className="grid-container">
-            <div className="title"><h1 style={{color:"white" }}>Todo List</h1></div>
+            <div className="title"><div><Alarm/></div><h1 style={{color:"white" }}>Todo List</h1></div>
                <div className="todo">
 
                   <div style={{marginLeft:"63px", marginBottom:"20px"}}>
-                  <Row>
-                         <Input type="text" className="firstinput" placeholder="what do you do?" name="inputNew" value={this.state.inputNew} onChange={(e) => HandleChange.call(this, e)}/>
+                     <Row>
+                         <Input type="text" className="firstinput" placeholder="what do you want to do?" name="inputNew" value={this.state.inputNew} onChange={(e) => HandleChange.call(this, e)}/>
                          <Button className="btn-add" onClick={this.add} children={<Icon icon={'plus'}/>}/>                   
-                  </Row>  
+                    </Row> 
+                  </div>
 
-                  <Nav tabs>
-                    <NavItem className="nav1">
-                      <NavLink onClick={() => this.setState({show: true })}>
-                        undone
-                      </NavLink>
-                    </NavItem>
-                    <NavItem className="nav1">
-                      <NavLink onClick={() => this.setState({show: false})}>
-                        done
-                      </NavLink>
-                    </NavItem>
-                  </Nav>
-
-                 </div>
-
-         {this.state.show? 
-                 <div >
+              
+                 <div>
                   
                        <ListGroup>
                          
                          {
-                           this.state.data.map((el, index) => {
-                             return <ListGroupItem key={index} style={{marginBottom:"5px", padding:"7px"}}>
-                                        <input type="checkbox"></input>
+                           this.state.data.map((el, index) => {                            
+                             return <ListGroupItem key={index}  style={{marginBottom:"5px", padding:"7px"}}>
+                                        <input type="checkbox" style={{marginRight:"7px", marginTop:"11px"}}></input>
                                         {el.title}
                                         <Button onClick={this.delete.bind(this, index)} className="float-right" style={{marginLeft:"5px"}} color="danger" children={<Icon icon={'trash'}/>}/>
                                         <Button onClick={this.edit.bind(this, index)} className="float-right" style={{marginLeft:"5px"}} color="info" children={<Icon icon={'pencil'}/>}/>
-                                        <Button onClick={this.done.bind(this, index)} className="float-right" color="success" children={<Icon icon={'check'}/>}/>
                                     </ListGroupItem> 
                            })
                          }
                          
-                       </ListGroup>
-                      
-                 </div> : 
+                       </ListGroup>                     
+                 </div>             
                 
-                 <div>
-
-                   <Input/>
-                   </div>
                
-                 }
-
-
-
                </div>
               </div>
+
+              <div style={{
+                 backgroundColor:"#28A745",
+                 color: "white",
+                 padding: "16px",
+                 position: "absolute",
+                 top: `${this.state.top}px` ,
+                 right: "16px",
+                 zIndex: 100,
+                 transition: "top 0.5s ease"
+                         }} > <Icon icon={'bell'}/> your item successfully deleted </div>
      </React.Fragment>
     )
   }
